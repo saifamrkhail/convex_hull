d009e4605af3e3ec0dac2d83ac03787f026f6cf5
ai22m048 <114658884+ai22m048@users.noreply.github.com>
2022-10-11 15:31:48 +0200

    Update maincpp.cpp

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,9 +1,16 @@
+#include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
+#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace cv;
 using namespace std;
+
+int g_switch_value = 0;
+int g_switch_value_performance = 0;
 
 static void help(char** argv)
 {
@@ -11,6 +18,25 @@ static void help(char** argv)
         << "Call:\n"
         << argv[0] << endl;
 }
+
+void switch_callback(int position, void*) {
+    if (position == 0) {
+        cout << "\nOpenCV" << endl;
+    }
+    else {
+        cout << "\nGraham" << endl;
+    }
+}
+
+void switch_callback_perf(int position, void*) {
+    if (position == 0) {
+        cout << "\nVisual" << endl;
+    }
+    else {
+        cout << "\nPerformance" << endl;
+    }
+}
+
 int main(int argc, char** argv)
 {
     CommandLineParser parser(argc, argv, "{help h||}");
@@ -20,6 +46,26 @@ int main(int argc, char** argv)
         return 0;
     }
     Mat img(500, 500, CV_8UC3);
+
+    // Name the main window
+    namedWindow("Convex Hull", 1);
+
+    createTrackbar(
+        "CV G D&C",
+        "Convex Hull",
+        &g_switch_value,
+        2,
+        switch_callback
+    );
+
+    createTrackbar(
+        "Vis Perf",
+        "Convex Hull",
+        &g_switch_value_performance,
+        1,
+        switch_callback_perf
+    );
+
     RNG& rng = theRNG();
     for (;;)
     {
@@ -38,7 +84,8 @@ int main(int argc, char** argv)
         for (i = 0; i < count; i++)
             circle(img, points[i], 3, Scalar(0, 0, 255), FILLED, LINE_AA);
         polylines(img, hull, true, Scalar(0, 255, 0), 1, LINE_AA);
-        imshow("convex hull", img);
+
+        imshow("Convex Hull", img);
         char key = (char)waitKey();
         if (key == 27 || key == 'q' || key == 'Q') // 'ESC'
             break;
