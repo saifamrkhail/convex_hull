f34628e1546276743d2876718e2836f9234b856b
DerCodenascher <49376631+DerCodenascher@users.noreply.github.com>
2022-10-12 12:00:36 +0200

    Update Giftwrapping.h

--- Giftwrapping.h
+++ Giftwrapping.h
@@ -1,10 +1,14 @@
 #include <vector>
 #include <iostream>
 #include <opencv2/core/types.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+#include "Timing.h"
 #pragma once
 
 class Giftwrapping {
 public:
 	static void convexHull(std::vector<cv::Point2f> points, std::vector<cv::Point2f>& hull);
+	static void convexHullVis(std::vector<cv::Point2f> points, std::vector<cv::Point2f>& hull, cv::Mat& matrix);
 	static float orient2d(cv::Point2f a, cv::Point2f b, cv::Point2f c);
 };
